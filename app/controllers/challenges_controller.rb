64c0b60997a3310b296e0d28ff843647bc2a3b74
baottran <bowtran@gmail.com>
2015-05-06 18:11:58 -0700

    can successfully post challenges

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -1,10 +1,29 @@
 class ChallengesController < ApplicationController
 	def index
 		@challenges = Challenge.all
+		@new_challenge = Challenge.new
+		current_user 
+		# p "GOTCHA" * 10
+		p @current_user
+
 	end
 
 	def create
-		p "GOTCHA" * 10
+		p "==================" * 10
+		p params
+		p "==================" * 10
+		current_user
+		p @current_user
+		p "==================" * 10
+		newchallenge = Challenge.create(title: params["challenge"]["title"], description: params["challenge"]["description"], proposer_id: @current_user.id)
+		# newchallenge.save
+		p "==================" * 10
+		p "NEW CHALLENGE"
+		p newchallenge
+		p "==================" * 10
+		# newchallenge.proposer = params.challenge.
+
+		redirect_to :root
 	end
 	
 end
